c193ab7f064cd726c4a3e1b534d5829a316ab0c7
sklevzhic <sklevzhic@gmail.com>
2023-01-31 20:57:27 +0300

    feat: added prettier

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,8 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
-root.render(
-    <div>Banking</div>
-);
+root.render(<div>Banking</div>);
 
 reportWebVitals();
