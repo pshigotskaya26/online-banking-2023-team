02edda742ba01ca43c4f83b496aa1082eb1795fa
Pavel Bandaronak <p_bandaronak@locport.io>
2023-02-01 21:33:23 +0300

    feat: add basic layout for dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,16 @@
 import ClientLayout from '../../layouts/client';
-import React from 'react';
+import React, { useState } from "react";
+import ICard from "../../types/interfaces/ICard";
+import { ITransaction } from "../../types/interfaces/ITransaction";
 
 const DashboardPage = (): JSX.Element => {
+  const [cards, setCards] = useState<ICard[]>([]);
+  const [transactions, setTransactions] = useState<ITransaction[]>([]);
+
   return (
     <ClientLayout>
-      <div>Контент</div>
+      {/*<CardList cards={cards}></CardList>*/}
+      {/*<TransactionList transactions={transactions}></TransactionList>*/}
     </ClientLayout>
   );
 };
