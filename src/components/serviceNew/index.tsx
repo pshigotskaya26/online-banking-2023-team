4795ae41d09cec278a03a6795a521b5d8b7dc3ea
sklevzhic <sklevzhic@gmail.com>
2023-02-02 23:16:56 +0300

    feat: added layout admin page

---
+++ src/components/serviceNew/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ServiceNew = () => {
+  return <div
+    className="w-full p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 dark:bg-gray-800 dark:border-gray-700">
+    <h5 className="mb-3 text-base font-semibold text-gray-900 md:text-xl dark:text-white">
+      Создать, обновить
+    </h5>
+
+  </div>
+}
+
+export default ServiceNew
