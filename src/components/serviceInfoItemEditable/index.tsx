e2beb629f47826f0e1cac75cb4ab108648c6f4f2
sklevzhic <sklevzhic@gmail.com>
2023-02-04 16:35:33 +0300

    feat: added component ServiceInfoItemEditable

---
+++ src/components/serviceInfoItemEditable/index.tsx
@@ -0,0 +1,28 @@
+import React, {FC, useState} from "react";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+interface ServiceInfoItemEditableProps {
+  value: string,
+  title: string | number | boolean,
+  icon: IconProp
+}
+
+const ServiceInfoItemEditable: FC<ServiceInfoItemEditableProps> = ({title, value, icon}) => {
+  const [editMode, setEditMode] = useState<boolean>(false)
+
+  return <div className="flex w-full mb-2 items-center space-x-3 border rounded-2xl p-0.5">
+    <div className={"basis-1/12 text-center"}>
+      <FontAwesomeIcon icon={icon} size={"lg"} className={"text-blue-600"}/>
+    </div>
+    <div className="font-medium grow dark:text-white text-left">
+      <div className="text-sm font-light text-gray-500 dark:text-gray-400"> {value} </div>
+      <div>{title}</div>
+    </div>
+    <div className={"basis-1/12"}>
+      <button>C</button>
+    </div>
+  </div>
+}
+
+export default ServiceInfoItemEditable
