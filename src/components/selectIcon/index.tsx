1260ed65a658f9b9751ef8e386a3a5370de7fac1
sklevzhic <sklevzhic@gmail.com>
2023-02-05 20:15:35 +0300

    feat: added component for select icon

---
+++ src/components/selectIcon/index.tsx
@@ -0,0 +1,56 @@
+import {IconLookup} from "@fortawesome/fontawesome-svg-core";
+import {
+  faBuildingShield,
+  faClose,
+  faEdit,
+  faPenClip,
+  faFloppyDisk,
+  faReceipt,
+  faHashtag,
+  faCheck,
+  faHouse,
+  faLandmark,
+  faAppleAlt,
+  faAdd,
+  faAngleRight,
+  faAnglesUp,
+  faDeafness,
+} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import React, {FC} from "react";
+
+
+const iconsData = [faReceipt, faDeafness, faBuildingShield, faClose, faEdit, faPenClip, faFloppyDisk, faHashtag, faCheck, faHouse, faLandmark, faAppleAlt, faAdd, faAngleRight, faAnglesUp]
+
+
+interface SelectIconProps {
+  icons?: IconLookup[],
+  activeIcon?: IconLookup | undefined,
+  handleChangeIcon: (icon: IconLookup) => void
+}
+
+const SelectIcon: FC<SelectIconProps> = (props) => {
+  const {icons = iconsData, handleChangeIcon, activeIcon = icons[0]} = props
+  debugger
+  const handleActiveIcon = (e: React.MouseEvent<HTMLButtonElement>,icon: IconLookup) => {
+    e.preventDefault()
+    handleChangeIcon(icon)
+  }
+
+  return <div className={"flex"}>
+    {icons.map((icon) => {
+      return <button key={icon.iconName}
+
+                     onClick={(e: React.MouseEvent<HTMLButtonElement>) => handleActiveIcon(e, icon)}
+        className={` mr-1 p-2 w-14 rounded-xl ${icon.iconName === activeIcon?.iconName && "bg-gray-200"}`}>
+          <FontAwesomeIcon
+          icon={icon}
+          key={icon.iconName}
+        />
+      </button>
+    })}
+  </div>
+
+}
+
+export default SelectIcon
