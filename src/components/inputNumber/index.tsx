a7ce7f4e0808a68a13fb5e0863d029b1f398c6f6
sklevzhic <sklevzhic@gmail.com>
2023-02-05 20:15:52 +0300

    feat: added component input number

---
+++ src/components/inputNumber/index.tsx
@@ -0,0 +1,42 @@
+import React, {FC} from "react";
+
+interface InputNumberProps {
+  label: string,
+  name: string,
+  value: number,
+  setValue: (v: number) => void,
+  type?: "number" | "text"
+
+}
+
+const InputNumber: FC<InputNumberProps> = (props) => {
+  const {
+    label,
+    name,
+    value,
+    setValue,
+  } = props
+  const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(+e.target.value)
+  }
+
+  const handleKeyPressValue = (event: React.KeyboardEvent<HTMLInputElement>) => {
+
+  }
+
+
+  return <>
+    <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Label</label>
+    <input type="number" min={"0"} value={value}
+           className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600
+           focus:border-primary-600 block w-full dark:bg-gray-700 dark:border-gray-600
+           dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500
+           p-2.5 ${value === 0 && "text-gray-50"}  
+           `}
+           onChange={handleInputValue}
+           onKeyDown={handleKeyPressValue}
+    />
+  </>
+}
+
+export default InputNumber
