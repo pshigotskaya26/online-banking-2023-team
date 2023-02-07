8ddd5d831c918b1310b5810262ff8f2975d038c3
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2023-02-07 14:29:46 +0300

    feat: create enum for card background

---
+++ src/types/enums/CardBackgroundEnum.ts
@@ -0,0 +1,9 @@
+enum CardBackgroundEnum {
+	color_blue = 'card-bg-1',
+	color_green = 'card-bg-2',
+	color_orange = 'card-bg-3',
+	color_violett = 'card-bg-4',
+	color_red = 'card-bg-5'
+}
+
+export default CardBackgroundEnum;
