30f31dc301de14c3cd4487e81e316118e0805c4e
p_bondarenok <p_bondarenok@lesta.group>
2023-02-08 16:28:58 +0300

    fix: add login form autofill

--- src/components/loginForm/index.tsx
+++ src/components/loginForm/index.tsx
@@ -41,6 +41,7 @@ const LoginForm: React.FC = () => {
             </label>
             <input
               type="email"
+              name="email"
               className="email-input"
               value={login}
               onChange={(e) => setLogin(e.target.value)}
@@ -55,6 +56,7 @@ const LoginForm: React.FC = () => {
             </label>
             <input
               type="password"
+              name="password"
               className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
