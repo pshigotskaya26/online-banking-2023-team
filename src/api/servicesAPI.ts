5033f60ef21b0940e1ec76430266ccb0e2d799a2
p_bandaronak <pavel.bondarenok@gmail.com>
2023-02-09 19:55:33 +0300

    fix: refactor servicesAPI and utilites

--- src/api/servicesAPI.ts
+++ src/api/servicesAPI.ts
@@ -1,75 +1,87 @@
-import IService from "../types/interfaces/IService";
+import IService from '../types/interfaces/IService';
 
 // import servicesData from "../data/services";
 
 class ServicesAPI {
   fetchServices(): IService[] {
-    let res: IService[] = []
-    if (localStorage.getItem("services")) {
-      const data = JSON.parse(localStorage.getItem("services") || "")
-      if (Array.isArray(data)) res = data
+    let res: IService[] = [];
+    if (localStorage.getItem('services')) {
+      const data = JSON.parse(localStorage.getItem('services') || '');
+      if (Array.isArray(data)) res = data;
     } else {
-      res = []
+      res = [];
     }
-    return res
+    return res;
   }
 
   createService(serviceData: IService): IService | Error {
-    if (!localStorage.getItem("services")) {
-      let servicesNew: IService[] = [serviceData]
-      localStorage.setItem("services", JSON.stringify(servicesNew))
-      return serviceData
+    if (!localStorage.getItem('services')) {
+      const servicesNew: IService[] = [serviceData];
+      localStorage.setItem('services', JSON.stringify(servicesNew));
+      return serviceData;
     } else {
-      let services = JSON.parse((localStorage.getItem("services") || "")) as IService[]
-      let servicesWithCode = services.find(service => service.code === serviceData.code)
+      const services = JSON.parse(
+        localStorage.getItem('services') || '',
+      ) as IService[];
+      const servicesWithCode = services.find(
+        (service) => service.code === serviceData.code,
+      );
 
       if (!servicesWithCode) {
-        let servicesNew: IService[] = [...services, serviceData]
-        localStorage.setItem("services", JSON.stringify(servicesNew))
-        return serviceData
+        const servicesNew: IService[] = [...services, serviceData];
+        localStorage.setItem('services', JSON.stringify(servicesNew));
+        return serviceData;
       } else {
-        throw new Error("A service with this code exists")
+        throw new Error('A service with this code exists');
       }
     }
   }
 
   deleteService(id: number) {
-    let services = JSON.parse((localStorage.getItem("services") || "")) as IService[]
-    let servicesNew: IService[] = services.filter(service => service.id !== id)
-    localStorage.setItem("services", JSON.stringify(servicesNew))
+    const services: IService[] = JSON.parse(
+      localStorage.getItem('services') || '[]',
+    );
+    const servicesNew: IService[] = services.filter(
+      (service) => service.id !== id,
+    );
+    localStorage.setItem('services', JSON.stringify(servicesNew));
   }
-
 
   updateService(serviceData: IService): IService | Error {
-    let services = JSON.parse((localStorage.getItem("services") || "")) as IService[];
-    const currentService = services.find(el => el.id === serviceData.id) as IService;
-    let restServicesCodes = services.filter(service => service.id !== serviceData.id).map(service => service.code);
-    const newCode = serviceData.code
+    const services: IService[] = JSON.parse(
+      localStorage.getItem('services') || '[]',
+    );
+    const currentService = services.find((el) => el.id === serviceData.id);
+    const restServicesCodes = services
+      .filter((service) => service.id !== serviceData.id)
+      .map((service) => service.code);
+    const newCode = serviceData.code;
 
     if (restServicesCodes.includes(newCode)) {
-      throw new Error("A service with this code exists")
+      throw new Error('A service with this code exists');
     } else {
-      const tempServices = services.map(service => service.id === currentService.id ? serviceData : service)
-      localStorage.setItem("services", JSON.stringify(tempServices))
+      const tempServices = services.map((service) =>
+        service.id === currentService?.id ? serviceData : service,
+      );
+      localStorage.setItem('services', JSON.stringify(tempServices));
     }
 
-    return serviceData
-
+    return serviceData;
   }
-
 
   handleAvailabilityService(id: number) {
-    let services = JSON.parse((localStorage.getItem("services") || "")) as IService[]
+    const services: IService[] = JSON.parse(
+      localStorage.getItem('services') || '[]',
+    );
 
-    let servicesNew = services.map(service => {
+    const servicesNew = services.map((service) => {
       if (service.id === id) {
-        return {...service, isAvailable: !service.isAvailable}
+        return { ...service, isAvailable: !service.isAvailable };
       }
-      return service
-    })
-    localStorage.setItem("services", JSON.stringify(servicesNew))
+      return service;
+    });
+    localStorage.setItem('services', JSON.stringify(servicesNew));
   }
-
 }
 
 const servicesAPI = new ServicesAPI();
--- src/utils/formateCardData.ts
+++ src/utils/formateCardData.ts
@@ -1,70 +1,66 @@
-import { IClientUser } from "../types/interfaces/IUser";
-import { faEye } from "@fortawesome/free-solid-svg-icons";
-import { faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { IClientUser } from '../types/interfaces/IUser';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 export const getStringCardNumber = (cardNumber: number) => {
-	let resultStringCardNumber = '';
-	let arrStringCardNumber = cardNumber.toString().split('');
-
-	for (let i = 0; i < arrStringCardNumber.length; i++) {
-		if (i === 3 || i === 7 || i === 11) {
-			resultStringCardNumber += arrStringCardNumber[i] + ' ';
-		}
-		else {
-			resultStringCardNumber += arrStringCardNumber[i];
-		}
-	}
-
-	return resultStringCardNumber;
+  return (
+    cardNumber
+      .toString()
+      .match(/.{1,4}/g)
+      ?.join(' ') ?? ''
+  );
 };
 
 export const getStringCardBalance = (cardBalance: number) => {
-	return cardBalance.toString();
+  return cardBalance.toString();
 };
 
 export const getCardUserById = (arrUsers: IClientUser[], idProps: number) => {
-	return arrUsers.filter((user: IClientUser) => user.id === idProps)[0];
+  return arrUsers.filter((user: IClientUser) => user.id === idProps)[0];
 };
 
 export const generateCardNumber = () => {
-	let startStr = '';
+  let startStr = '';
 
-	for (let i = 0; i < 16; i++) {
-		let randomNumber = Math.floor(Math.random() * 10);
-		startStr += randomNumber;
-	}
+  for (let i = 0; i < 16; i++) {
+    let randomNumber = Math.floor(Math.random() * 10);
+    startStr += randomNumber;
+  }
 
-	return Number(startStr);
+  return Number(startStr);
 };
 
 export const changeIconEyeInCard = (isShownValue: boolean) => {
-	let iconEye = (isShownValue) ? faEye : faEyeSlash;
-	return iconEye;
-}
+  let iconEye = isShownValue ? faEye : faEyeSlash;
+  return iconEye;
+};
 
 export const changeNumberToStar = (stringNumber: string) => {
-	return `**** **** **** ${stringNumber.slice(-4)}`;
+  return `**** **** **** ${stringNumber.slice(-4)}`;
 };
 
 export const changeBalanceToStar = (stringNumber: string) => {
-	return '*'.repeat(stringNumber.length)
+  return '*'.repeat(stringNumber.length);
 };
 
-export const changeCardNumber = (isShownValue: boolean, stringNumber: string) => {
+export const changeCardNumber = (
+  isShownValue: boolean,
+  stringNumber: string,
+) => {
+  if (isShownValue) {
+    return stringNumber;
+  } else {
+    return changeNumberToStar(stringNumber);
+  }
+};
 
-	if (isShownValue) {
-		return stringNumber;
-	}
-	else {
-		return changeNumberToStar(stringNumber);
-	}
-}
-
-export const changeCardBalance = (isShownValue: boolean, stringNumber: string) => {
-	if (isShownValue) {
-		return stringNumber;
-	}
-	else {
-		return changeBalanceToStar(stringNumber);
-	}
+export const changeCardBalance = (
+  isShownValue: boolean,
+  stringNumber: string,
+) => {
+  if (isShownValue) {
+    return stringNumber;
+  } else {
+    return changeBalanceToStar(stringNumber);
+  }
 };
