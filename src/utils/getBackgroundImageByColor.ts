be350f98262b7c2d52ced66f224cafd3ec49ec64
p_bandaronak <pavel.bondarenok@gmail.com>
2023-02-09 19:43:09 +0300

    fix: refactor getBackgroundImageByColor

--- src/utils/getBackgroundImageByColor.ts
+++ src/utils/getBackgroundImageByColor.ts
@@ -1,34 +1,21 @@
-import imgBlue from "../assets/cardBackground/card-bg-blue.png";
-import imgGreen from "../assets/cardBackground/card-bg-green.png";
-import imgOrange from "../assets/cardBackground/card-bg-orange.png";
-import imgViolett from "../assets/cardBackground/card-bg-violett.png";
-import imgRed from "../assets/cardBackground/card-bg-red.png";
-import CardBackgroundEnum from "../types/enums/CardBackgroundEnum";
+import imgBlue from '../assets/cardBackground/card-bg-blue.png';
+import imgGreen from '../assets/cardBackground/card-bg-green.png';
+import imgOrange from '../assets/cardBackground/card-bg-orange.png';
+import imgViolett from '../assets/cardBackground/card-bg-violett.png';
+import imgRed from '../assets/cardBackground/card-bg-red.png';
+import CardBackgroundEnum from '../types/enums/CardBackgroundEnum';
 
 export const getBackgroundImageByColor = (cardBackground: string) => {
-	let iamgBackground;
-
-	switch(cardBackground) {
-		case `${CardBackgroundEnum.blue}`:
-			iamgBackground = imgBlue;
-			break;
-
-		case `${CardBackgroundEnum.green}`:
-			iamgBackground = imgGreen;
-			break;
-		
-		case `${CardBackgroundEnum.orange}`:
-			iamgBackground = imgOrange;
-			break;
-
-		case `${CardBackgroundEnum.violett}`:
-			iamgBackground = imgViolett;
-			break;
-
-		case `${CardBackgroundEnum.red}`:
-			iamgBackground = imgRed;
-			break;
-	}
-
-	return iamgBackground;
+  switch (cardBackground) {
+    case CardBackgroundEnum.blue:
+      return imgBlue;
+    case CardBackgroundEnum.green:
+      return imgGreen;
+    case CardBackgroundEnum.orange:
+      return imgOrange;
+    case CardBackgroundEnum.violett:
+      return imgViolett;
+    default:
+      return imgRed;
+  }
 };
