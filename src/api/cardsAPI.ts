26e84a9822ecb5d85aa81a34aa40f385d124ada2
p_bandaronak <pavel.bondarenok@gmail.com>
2023-02-11 15:27:19 +0300

    feat: add method addUserCard to cardsAPI

--- src/api/cardsAPI.ts
+++ src/api/cardsAPI.ts
@@ -6,12 +6,21 @@ class CardsAPI {
     return cards.filter((card) => card.userid === userid);
   }
 
-  updateUserCards(userid: number, newCards: ICard[]): void {
+  updateCards(newCards: ICard[]): ICard[] {
     const cards: ICard[] = JSON.parse(localStorage.getItem('cards') ?? '[]');
     const newCardList = cards
-      .filter((card) => card.userid !== userid)
+      .filter((card) => newCards.every((cd) => cd.id !== card.id))
       .concat(newCards);
     localStorage.setItem('cards', JSON.stringify(newCardList));
+    return newCards;
+  }
+
+  addUserCard(newCard: ICard): ICard[] {
+    //we need to save continuous numbering
+    const cards: ICard[] = JSON.parse(localStorage.getItem('cards') ?? '[]');
+    newCard.id = cards.push(newCard);
+    localStorage.setItem('cards', JSON.stringify(cards));
+    return cards.filter((card) => card.userid === newCard.userid);
   }
 }
 
