e79fa4100419ce3f3db6274f5a25df950511d09f
p_bondarenok <p_bondarenok@lesta.group>
2023-02-14 22:34:47 +0300

    feat: show payments list

--- src/pages/payments/components/paymentList/index.tsx
+++ src/pages/payments/components/paymentList/index.tsx
@@ -1,9 +1,50 @@
 import { FC } from 'react';
+import IService from '../../../../types/interfaces/IService';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 
 interface PaymentListProps {
-  items: string[];
+  services: IService[];
 }
 
-export const PaymentList: FC<PaymentListProps> = () => {
-  return <></>;
+export const PaymentList: FC<PaymentListProps> = ({ services }) => {
+  if (services.length === 0) {
+    return <div>There is no services for display.</div>;
+  }
+
+  return (
+    <ul>
+      {services.map((service) => (
+        <li
+          key={service.id}
+          className={`my-3 bg-blue-200 rounded-2xl ${
+            service.isAvailable ? '' : 'opacity-50'
+          }`}
+        >
+          <Link
+            to={`${service.code}`}
+            className={`flex p-3 ${
+              service.isAvailable ? 'cursor-pointer' : 'cursor-not-allowed'
+            }`}
+            onClick={(e: React.MouseEvent) => {
+              if (!service.isAvailable) e.preventDefault();
+            }}
+          >
+            <div className={'mx-3 flex items-center justify-center w-1/12'}>
+              {service.icon && (
+                <FontAwesomeIcon
+                  icon={service.icon}
+                  size={'4x'}
+                ></FontAwesomeIcon>
+              )}
+            </div>
+            <div className={'mx-3 text-xl flex items-center'}>
+              {service.title}
+            </div>
+            <div className={'mx-3'}></div>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 };
--- src/pages/payments/index.tsx
+++ src/pages/payments/index.tsx
@@ -1,25 +1,61 @@
 import PageTitle from '../../components/pageTitle';
 import ClientLayout from '../../layouts/client';
-import { Tabs } from './components/Tabs/Tabs';
+import { TabsPanel } from './components/tabs';
 import { PaymentList } from './components/paymentList';
 import { useEffect, useState } from 'react';
+import { useActions } from '../../hooks/useActions';
+import { useAppSelector } from '../../hooks/useAppSelector';
+import PaymentTabsNamesEnum from '../../types/enums/PaymentTabsNamesEnum';
+import { Route, Routes } from 'react-router-dom';
+import { PaymentForm } from './components/paymentForm';
+import IService from '../../types/interfaces/IService';
 
 const PaymentsPage = () => {
-  const [tab, setTab] = useState('');
-  const [payments, setPayments] = useState([]);
-  const tabChangeHandler = (tabName: string) => {
-    setTab(tabName);
+  const { user } = useAppSelector((state) => state.authuser);
+  const { services } = useAppSelector((state) => state.services);
+
+  const [payments, setPayments] = useState<IService[]>([]);
+
+  const filterFavoriteServices = () =>
+    services.filter((service) => user?.favoriteServices.includes(service.id));
+  const tabChangeHandler = (tabName: PaymentTabsNamesEnum) => {
+    let srvcs = services;
+    switch (tabName) {
+      case PaymentTabsNamesEnum.FAVORITE:
+      case PaymentTabsNamesEnum.AUTO_PAUMENTS:
+        srvcs = filterFavoriteServices();
+        break;
+      case PaymentTabsNamesEnum.POPULAR:
+        break;
+    }
+    setPayments(srvcs);
   };
 
+  const { fetchServices } = useActions();
   useEffect(() => {
-    setPayments([]);
-  }, [tab]);
+    fetchServices();
+  }, []);
+
+  useEffect(() => {
+    setPayments(filterFavoriteServices());
+  }, [services]);
 
   return (
     <ClientLayout>
       <PageTitle title={'Payments'} description={''} />
-      <Tabs callback={tabChangeHandler}></Tabs>
-      <PaymentList items={payments}></PaymentList>
+      <Routes>
+        <Route
+          path={'/'}
+          element={
+            <>
+              {' '}
+              <TabsPanel callback={tabChangeHandler}></TabsPanel>{' '}
+              <PaymentList services={payments}></PaymentList>
+            </>
+          }
+        ></Route>
+        <Route path={'/:id'} element={<PaymentForm />}></Route>
+      </Routes>
     </ClientLayout>
   );
 };
