d9e98be59c950424eff7cf1d1d9f226349255f90
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2023-02-16 15:57:39 +0300

    feat: add button named Take a credit in dashboard page

--- src/components/cardList/index.tsx
+++ src/components/cardList/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './index.css';
 import ICard from '../../types/interfaces/ICard';
+import ICredit from '../../types/interfaces/ICredit';
 import CardItem from '../cardItem';
 import { useAppSelector } from '../../hooks/useAppSelector';
 import { useActions } from '../../hooks/useActions';
@@ -11,9 +12,11 @@ interface CardListProps {}
 const CardList: React.FC<CardListProps> = (props) => {
   const { user } = useAppSelector((state) => state.authuser);
   const { cards: userCards } = useAppSelector((state) => state.usercards);
+  const { credits: userCredits } = useAppSelector((state) => state.usercredits);
 
   const { getCardsByUserId } = useActions();
   const [cards, setCards] = useState<ICard[]>([]);
+  const [credits, setCredits] = useState<ICredit[]>([]);
 
   useEffect(() => {
     if (user !== null) {
@@ -25,15 +28,23 @@ const CardList: React.FC<CardListProps> = (props) => {
     setCards(userCards);
   }, [userCards]);
 
+  useEffect(() => {
+    setCredits(userCredits);
+  }, [userCredits]);
+
   return (
     <div className="cardList-container">
       <div className="cardList__head">
         <h2 className="cardList__title">Card list:</h2>
-        <button className="button button-add">
-          <Link to={'/new-card'}>Add card</Link>
-        </button>
+        <div className="cardList__buttons">
+          <button className="button button-add">
+            <Link to={'/new-card'}>Add card</Link>
+          </button>
+          <button className="button button-take-credit">
+            <Link to={'/take-credit'}>Take a credit</Link>
+          </button>
+        </div>
       </div>
-
       <div className="cardList__content">
         {cards.map((cardItem: ICard) => (
           <CardItem key={cardItem.id} card={cardItem} />
