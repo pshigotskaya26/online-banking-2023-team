d3fd93153e3e366d09e3cdd303c2c5507d804774
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2023-02-17 16:25:33 +0300

    feat: create a credit form

--- src/components/formNewCredit/index.tsx
+++ src/components/formNewCredit/index.tsx
@@ -3,20 +3,85 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../../hooks/useAppSelector';
 import CreditTermEnum from '../../types/enums/CreditTermEnum';
+import CreditThingEnum from '../../types/enums/CreditThingEnum';
+import CreditSumEnum from '../../types/enums/CreditSumEnum';
 
 const FormNewCredit: React.FC = () => {
   const { user } = useAppSelector((state) => state.authuser);
   console.log('user: ', user);
 
   const [creditTerm, setCreditTerm] = useState<string>(CreditTermEnum.TEN_DAYS);
+  const [creditThing, setCreditThing] = useState<string>(
+    CreditThingEnum.BICYCLE,
+  );
+
+  const [creditSum, setCreditSum] = useState<string>(CreditSumEnum.SUM_10000);
 
   return (
     <div className="new-credit__form">
-      <h5 className="mb-5 text-base font-semibold text-gray-900 md:text-xl dark:text-white">
+      <h4 className="mb-5 text-base font-semibold text-gray-900 md:text-xl dark:text-white">
         Take a new credit:
-      </h5>
+      </h4>
       <div className="new-credit__container">
-        <div className="new-credit__content">1</div>
+        <div className="new-credit__content">
+          <div className="new-credit__user-data w-1/5">
+            <p className="mb-4 text-sm font-medium text-gray-900 dark:text-white h-8">
+              Your name / surname:{' '}
+            </p>
+            <p className="user-data__value">{user?.name}</p>
+          </div>
+
+          <div className="new-credit__entity w-1/5">
+            <p className="mb-4 text-sm font-medium text-gray-900 dark:text-white h-8">
+              Select a thing you want to take in credit:
+            </p>
+            <select
+              className="select-thing w-full"
+              size={Object.keys(CreditThingEnum).length}
+              value={creditThing}
+            >
+              {Object.keys(CreditThingEnum).map((item: string) => (
+                <option key={item} value={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="new-credit__summ w-1/5">
+            <p className="mb-4 text-sm font-medium text-gray-900 dark:text-white h-8">
+              Enter sum you want to take in credit (BYN):
+            </p>
+            <select
+              className="select-sum w-full"
+              size={Object.values(CreditSumEnum).length}
+              value={creditSum}
+            >
+              {Object.values(CreditSumEnum).map((item: string) => (
+                <option key={item} value={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="new-credit__term w-1/5">
+            <p className="mb-4 text-sm font-medium text-gray-900 dark:text-white h-8">
+              Select a term of the credit (DAYS):
+            </p>
+            <select
+              className="select-term w-full"
+              size={Object.values(CreditTermEnum).length}
+              value={creditTerm}
+            >
+              {Object.values(CreditTermEnum).map((item: string) => (
+                <option key={item} value={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
         <div className="new-credit__buttons">
           <input
             className="button button-create-credit"
