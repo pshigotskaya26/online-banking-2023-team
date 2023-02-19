dc7c804eb2aaa55ea582e9accc487a086743620d
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2023-02-19 16:43:50 +0300

    feat: add addUserCredit in CreditsAPI

--- src/api/creditsAPI.ts
+++ src/api/creditsAPI.ts
@@ -3,9 +3,18 @@ import ICredit from '../types/interfaces/ICredit';
 class CreditsAPI {
   getCreditsByUserId(userid: number): ICredit[] {
     const credits: ICredit[] = JSON.parse(
-      localStorage.getItem('creditss') ?? '[]',
+      localStorage.getItem('credits') ?? '[]',
     );
     return credits.filter((credit) => credit.userId === userid);
+  }
+
+  addUserCredit(newCredit: ICredit): ICredit[] {
+    const credits: ICredit[] = JSON.parse(
+      localStorage.getItem('credits') ?? '[]',
+    );
+    newCredit.id = credits.push(newCredit);
+    localStorage.setItem('credits', JSON.stringify(credits));
+    return credits.filter((credit) => credit.userId === newCredit.userId);
   }
 }
 
--- src/store/actions/credits.ts
+++ src/store/actions/credits.ts
@@ -1,6 +1,7 @@
 import { Dispatch } from 'redux';
 import { CreditsActionTypes, CreditsManagementActions } from '../types/credits';
 import creditsAPI from '../../api/creditsAPI';
+import ICredit from '../../types/interfaces/ICredit';
 
 export const getCreditsByUserId = (userid: number) => {
   return (dispatch: Dispatch<CreditsManagementActions>) => {
@@ -9,6 +10,26 @@ export const getCreditsByUserId = (userid: number) => {
       const response = creditsAPI.getCreditsByUserId(userid);
       dispatch({
         type: CreditsActionTypes.FETCH_CREDITS_SUCCESS,
+        payload: response,
+      });
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        dispatch({
+          type: CreditsActionTypes.FETCH_CREDITS_ERROR,
+          payload: e.message,
+        });
+      }
+    }
+  };
+};
+
+export const addUserCredit = (newCredit: ICredit) => {
+  return (dispatch: Dispatch<CreditsManagementActions>) => {
+    try {
+      dispatch({ type: CreditsActionTypes.UPDATE_CREDITS });
+      const response = creditsAPI.addUserCredit(newCredit);
+      dispatch({
+        type: CreditsActionTypes.UPDATE_CREDITS_SUCCESS,
         payload: response,
       });
     } catch (e: unknown) {
