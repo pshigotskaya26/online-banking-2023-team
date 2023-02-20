aee0b2212a346a0359fda7a355d73fb9836ddfe4
pshigotskaya26 <pshigotskaya.tanya@tut.by>
2023-02-20 16:43:47 +0300

    feat: add button named Pay all forevery credit

--- src/components/creditItem/index.tsx
+++ src/components/creditItem/index.tsx
@@ -18,61 +18,66 @@ const CreditItem: React.FC<CreditItemProps> = (props) => {
         {props.credit.id}. Credit: {props.credit.entity}
       </h4>
       <div className="credit-item__description">
-        <h5 className="credit-description__title">A credit desciption:</h5>
-        <div className="credit-description__content">
-          <div className="credit-description credit-description__sum">
-            <div className="credit-description__text credit-description__sum-text">
-              Credit sum ({CardCurrencyEnum.BYN}):
+        <div className="credit-description__info">
+          <h5 className="credit-description__title">A credit desciption:</h5>
+          <div className="credit-description__content">
+            <div className="credit-description credit-description__sum">
+              <div className="credit-description__text credit-description__sum-text">
+                Credit sum ({CardCurrencyEnum.BYN}):
+              </div>
+              <div className="credit-description__value credit-description__sum-value">
+                {props.credit.summOfCredit}
+              </div>
             </div>
-            <div className="credit-description__value credit-description__sum-value">
-              {props.credit.summOfCredit}
-            </div>
-          </div>
 
-          <div className="credit-description credit-description__paid-sum">
-            <div className="credit-description__text credit-description__paid-sum-text">
-              Paid sum ({CardCurrencyEnum.BYN}):
+            <div className="credit-description credit-description__paid-sum">
+              <div className="credit-description__text credit-description__paid-sum-text">
+                Paid sum ({CardCurrencyEnum.BYN}):
+              </div>
+              <div className="credit-description__value credit-description__paid-sum-value">
+                {props.credit.summPaid}
+              </div>
             </div>
-            <div className="credit-description__value credit-description__paid-sum-value">
-              {props.credit.summPaid}
-            </div>
-          </div>
 
-          <div className="credit-description credit-description__remainder">
-            <div className="credit-description__text credit-description__remainder-text">
-              Remainder ({CardCurrencyEnum.BYN}):
+            <div className="credit-description credit-description__remainder">
+              <div className="credit-description__text credit-description__remainder-text">
+                Remainder ({CardCurrencyEnum.BYN}):
+              </div>
+              <div className="credit-description__value credit-description__remainder-value">
+                {props.credit.remainder}
+              </div>
             </div>
-            <div className="credit-description__value credit-description__remainder-value">
-              {props.credit.remainder}
-            </div>
-          </div>
 
-          <div className="credit-description credit-description__fine">
-            <div className="credit-description__text credit-description__fine-text">
-              Fine ({CardCurrencyEnum.BYN}):
+            <div className="credit-description credit-description__fine">
+              <div className="credit-description__text credit-description__fine-text">
+                Fine ({CardCurrencyEnum.BYN}):
+              </div>
+              <div className="credit-description__value credit-description__fine-value">
+                {props.credit.fine}
+              </div>
             </div>
-            <div className="credit-description__value credit-description__fine-value">
-              {props.credit.fine}
-            </div>
-          </div>
 
-          <div className="credit-description credit-description__term">
-            <div className="credit-description__text credit-description__term-text">
-              A credit term (DAY/DAYS):
+            <div className="credit-description credit-description__term">
+              <div className="credit-description__text credit-description__term-text">
+                A credit term (DAY/DAYS):
+              </div>
+              <div className="credit-description__value credit-description__term-value">
+                {props.credit.term}
+              </div>
             </div>
-            <div className="credit-description__value credit-description__term-value">
-              {props.credit.term}
-            </div>
-          </div>
 
-          <div className="credit-description credit-description__start-date">
-            <div className="credit-description__text credit-description__start-date-text">
-              A credit start date:
-            </div>
-            <div className="credit-description__value credit-description__start-date-value">
-              {getStringDate(props.credit.dateStart)}
+            <div className="credit-description credit-description__start-date">
+              <div className="credit-description__text credit-description__start-date-text">
+                A credit start date:
+              </div>
+              <div className="credit-description__value credit-description__start-date-value">
+                {getStringDate(props.credit.dateStart)}
+              </div>
             </div>
           </div>
+        </div>
+        <div className="credit-description__button">
+          <button className="button button-pay-all">Pay all</button>
         </div>
       </div>
 
