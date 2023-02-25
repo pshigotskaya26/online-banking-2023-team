5abb0795b0e1eba25c412d4d0401184d9e8ef73b
p_bandaronak <pavel.bondarenok@gmail.com>
2023-02-25 19:58:30 +0300

    fix: update service display on transactionitem

--- src/components/transactionItem/index.tsx
+++ src/components/transactionItem/index.tsx
@@ -5,6 +5,8 @@ import { getStringDate, getStringTime } from '../../utils/formateDateTime';
 import { useAppSelector } from '../../hooks/useAppSelector';
 import ICard from '../../types/interfaces/ICard';
 import { getCardsByUserId } from '../../store/actions/cardsManagement';
+import IService from '../../types/interfaces/IService';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface TransactionItemProps {
   transaction: ITransaction;
@@ -13,7 +15,10 @@ interface TransactionItemProps {
 const TransactionItem: React.FC<TransactionItemProps> = ({ transaction }) => {
   const { user } = useAppSelector((state) => state.authuser);
   const { cards: userCards } = useAppSelector((state) => state.usercards);
+  const { services } = useAppSelector((state) => state.services);
+
   const [card, setCard] = useState<ICard>({} as ICard);
+  const [service, setService] = useState<IService>({} as IService);
 
   useEffect(() => {
     if (user !== null) {
@@ -28,6 +33,15 @@ const TransactionItem: React.FC<TransactionItemProps> = ({ transaction }) => {
     }
   }, [userCards]);
 
+  useEffect(() => {
+    const serv = services.find(
+      (service) => service.id === transaction.entityid,
+    );
+    if (serv) {
+      setService(serv);
+    }
+  }, [services]);
+
   return (
     <tr className="transaction-item">
       <td className="transaction-item__orderNumb">{transaction.id}</td>
@@ -35,7 +49,10 @@ const TransactionItem: React.FC<TransactionItemProps> = ({ transaction }) => {
         <div>{getStringDate(transaction.timestamp)}</div>
         <div>{getStringTime(transaction.timestamp)}</div>
       </td>
-      <td className="transaction-item__service">{transaction.entityid}</td>
+      <td className="transaction-item__service">
+        <FontAwesomeIcon icon={service.icon!} size={'2x'} />{' '}
+        <span className={'align-super ml-3'}>{service.title}</span>
+      </td>
       <td className="transaction-item__type">{transaction.entitytype}</td>
       <td className="transaction-item__status">{transaction.status}</td>
       <td className="transaction-item__value">
